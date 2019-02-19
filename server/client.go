13ba2274e8aeb59113d4f2ee709d48c5161e2482
Parham Alvani <parham.alvani@gmail.com>
2019-02-19 13:17:30 +0330

    Completes client structure.

--- server/client.go
+++ server/client.go
@@ -1,13 +1,27 @@
 package server
 
-import "time"
+import (
+	"sync"
+	"time"
+
+	"github.com/tidwall/evio"
+)
 
 // Client is a remote connection into Tiles
 type Client struct {
-	id int
+	id         int              // unique id
+	remoteAddr string           // original remote address
+	in         evio.InputStream // input stream
+	pr         PipelineReader   // command reader
+	out        []byte           // output write buffer
 
-	remoteAddr string // original remote address
+	mu     sync.Mutex // guard
+	opened time.Time  // when the client was created/opened, unix nano
+	last   time.Time  // last client request/response, unix nano
+}
 
-	opened time.Time // when the client was created/opened, unix nano
-	last   time.Time // last client request/response, unix nano
+// Write writes on client output buffer
+func (client *Client) Write(b []byte) (n int, err error) {
+	client.out = append(client.out, b...)
+	return len(b), nil
 }
