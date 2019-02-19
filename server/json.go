266eb3711cb6d1b7b3c6be72885539d4681abb97
Parham Alvani <parham.alvani@gmail.com>
2019-02-19 14:36:13 +0330

    Adds tidwal json package.

---
+++ server/json.go
@@ -0,0 +1,17 @@
+package server
+
+import "encoding/json"
+
+func jsonString(s string) string {
+	for i := 0; i < len(s); i++ {
+		if s[i] < ' ' || s[i] == '\\' || s[i] == '"' || s[i] > 126 {
+			d, _ := json.Marshal(s)
+			return string(d)
+		}
+	}
+	b := make([]byte, len(s)+2)
+	b[0] = '"'
+	copy(b[1:], s)
+	b[len(b)-1] = '"'
+	return string(b)
+}
