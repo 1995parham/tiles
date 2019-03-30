599d33486d303e766aa0a63c30dc191f77712455
Parham Alvani <parham.alvani@gmail.com>
2019-03-30 12:02:22 +0430

    Removes unused function.

--- server/error.go
+++ server/error.go
@@ -10,7 +10,3 @@ var errInvalidNumberOfArguments = errors.New("invalid number of arguments")
 func errInvalidArgument(arg string) error {
 	return fmt.Errorf("invalid argument '%s'", arg)
 }
-
-func errDuplicateArgument(arg string) error {
-	return fmt.Errorf("duplicate argument '%s'", arg)
-}
