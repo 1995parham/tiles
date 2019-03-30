3dfec5e6125f3079c58997e570fe1de1aac2e510
Parham Alvani <parham.alvani@gmail.com>
2019-03-30 11:59:48 +0430

    Removes unused functions from token.

--- server/token.go
+++ server/token.go
@@ -9,15 +9,6 @@ func isReservedFieldName(field string) bool {
 	return false
 }
 
-func token(line string) (newLine, token string) {
-	for i := 0; i < len(line); i++ {
-		if line[i] == ' ' {
-			return line[i+1:], line[:i]
-		}
-	}
-	return "", line
-}
-
 // returns the first argument from the given string list and removes it from that.
 func tokenval(vs []string) (nvs []string, token string, ok bool) {
 	if len(vs) > 0 {
@@ -39,23 +30,6 @@ func tokenvalbytes(vs []string) (nvs []string, token []byte, ok bool) {
 }
 
 func lcb(s1 []byte, s2 string) bool {
-	if len(s1) != len(s2) {
-		return false
-	}
-	for i := 0; i < len(s1); i++ {
-		ch := s1[i]
-		if ch >= 'A' && ch <= 'Z' {
-			if ch+32 != s2[i] {
-				return false
-			}
-		} else if ch != s2[i] {
-			return false
-		}
-	}
-	return true
-}
-
-func lc(s1, s2 string) bool {
 	if len(s1) != len(s2) {
 		return false
 	}
